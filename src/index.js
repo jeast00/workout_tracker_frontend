5b5df0f402616d64cca5b7ec7a819ce29c9e3188
Jonathan East <jonathan_east@ymail.com>
2021-04-04 13:58:06 -0400

    added const variables, event listener, and function to index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // console.log("Test js page");
+
+// add variables to the top - hoisting
+const addWorkoutButton = document.getElementById('add_workout_button');
+const workoutForm = document.getElementById('workout_form_container');
+
+// console.log(addWorkoutButton); // checked
+
+// add an event listener to the addWorkoutButton variable to reveal the workout form
+addWorkoutButton.addEventListener('click', revealWorkoutForm)
+
+// add a function to reveal the workout form to the DOM
+function revealWorkoutForm() {
+    workoutForm.hidden = false;
+}
