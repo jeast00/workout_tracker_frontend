0e6a67a800315ab3cd6f22367e8bde9cc35ca70c
Jonathan East <jonathan_east@ymail.com>
2021-04-06 22:34:38 -0400

    changed variables to accomodate naming; finished saveExerciseInfo function; was able to assign each exercise to each workout correctly

--- src/index.js
+++ src/index.js
@@ -5,32 +5,25 @@ const addWorkoutButton = document.getElementById('add_workout_button');
 const workoutFormContainer = document.getElementById('workout_form_container');
 const workoutForm = document.getElementById('workout_form');
 const workoutNameInput = document.getElementById('workout_name_input');
+const workoutNameList = document.getElementById('workout_name_list');
 // const exerciseFormContainer = document.getElementById('exercise_form_container');
-const workoutExerciseContainer = document.getElementById('workout_exercise_container');
-const workoutListContainer = document.getElementById('workout_list_container');
-const workoutListDiv = document.getElementById('workout_list_div');
-const workoutList = document.getElementById('workout');
+// const workoutExerciseContainer = document.getElementById('workout_exercise_container');
+// const workoutListContainer = document.getElementById('workout_list_container');
+// const workoutListDiv = document.getElementById('workout_list_div');
+// const workoutList = document.getElementById('workout');
 
 // console.log(addWorkoutButton); // checked
 
-// add an event listener to the addWorkoutButton variable to reveal the workout form
-addWorkoutButton.addEventListener('click', revealWorkoutForm)
-
 // add an event listener to the workoutForm variable to hide the workoutForm, add elements to the DOM that show the workout Form input value, and then reveal the exercise form
 workoutForm.addEventListener('submit', saveWorkoutName)
-
-// add a function to reveal the workout form to the DOM
-function revealWorkoutForm() {
-    workoutFormContainer.hidden = false;
-}
 
 // add a function to add the Workout Name to the DOM, hide the workout form, and reveal the exercise form
 function saveWorkoutName() {
     event.preventDefault(); // if event listener is a 'submit' have a preventDefault function on the event
     // create elements to append to the DOM
-    const workoutNameDiv = document.createElement('div');
-    const workoutNameP = document.createElement('p');
-    workoutNameP.innerText = `Workout Name: ${workoutNameInput.value}`;
+    // const workoutNameDiv = document.createElement('div');
+    const workoutNameLI = document.createElement('li');
+    workoutNameLI.innerText = `Workout Name: ${workoutNameInput.value}`;
 
     // create form for exercise 
     const exerciseForm = document.createElement('form');
@@ -73,14 +66,15 @@ function saveWorkoutName() {
     
     exerciseForm.addEventListener('submit', saveExerciseInfo)
 
+    // create a unordered list element and append it with with workout
+    const exerciseList = document.createElement('ul');
+
     // append workout name to the DOM
-    workoutNameDiv.append(workoutNameP, exerciseForm);
-    workoutExerciseContainer.appendChild(workoutNameDiv);
+    // workoutNameDiv.append(workoutNameP, exerciseForm);
+    exerciseForm.appendChild(exerciseList);
+    workoutNameLI.appendChild(exerciseForm);
+    workoutNameList.appendChild(workoutNameLI);
 
-    
-
-    // hide the workout form from the DOM
-    workoutFormContainer.hidden = true;
 
     workoutForm.reset(); // resets the form after submission
 
@@ -94,7 +88,22 @@ function saveExerciseInfo(e) {
     const setInput = e.target.children[1].value
     const repInput = e.target.children[2].value
     const timeInput = e.target.children[3].value
+    const exerciseUL = e.target.children[5]
     
+    const exerciseNameLI = document.createElement('li');
+    exerciseNameLI.innerText = `Exercise Name: ${nameInput}`;
+
+    const exerciseSetLI = document.createElement('li');
+    exerciseSetLI.innerText = `Sets: ${setInput}`;
+
+    const exerciseRepLI = document.createElement('li')
+    exerciseRepLI.innerText = `Reps: ${repInput}`;
+
+    const exerciseTimeLI = document.createElement('li')
+    exerciseTimeLI.innerText = `Time: ${timeInput} minute(s)`;
+
+    exerciseUL.append(exerciseNameLI, exerciseSetLI, exerciseRepLI, exerciseTimeLI)
+
     
     // console.log(nameInput, setInput, repInput, timeInput);
 
